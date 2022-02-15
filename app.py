141e64f8b16cc6eb3655820832d7566cb3affea5
Erno <ernohor@gmail.com>
2022-02-15 20:23:01 +0100

    dop

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import json
 import math
 from random import Random
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, after_this_request
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+def mod(value):
+    return value
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return jsonify({"Name": "Pal", "Age": Random().randint(18, 80)})
+    if request.method == 'POST':
+        city = request.form.get('city')
+        return '''
+                <h1>The City is {}</h1>'''.format(city)
+
+    return '''
+              <form method="POST">
+              <center>
+                  <div><label>City: <input type="text" name="city"></label></div>
+                  <br></br>
+                  <input type="submit" value="Submit" style="align:center">
+                  </center>
+              </form>'''
 
 
 app.run()
