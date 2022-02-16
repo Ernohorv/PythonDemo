7d9a14004f8024b2fcc19f5040c7b31fa16c6fc6
Erno <ernohor@gmail.com>
2022-02-16 14:48:35 +0100

    end

--- app.py
+++ app.py
@@ -2,20 +2,39 @@ import json
 import math
 from random import Random
 from flask import Flask, request, jsonify, after_this_request
+import requests
 
 app = Flask(__name__)
-
-
-def mod(value):
-    return value
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         city = request.form.get('city')
-        return '''
-                <h1>The City is {}</h1>'''.format(city)
+        response = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID=6364d96eff37ce254aeabb6fcccd9740&units=metric")
+        content = json.loads(response.text)
+
+        if response.status_code == 200:
+            temp = content["main"]["temp"]
+            feels_like = content["main"]["feels_like"]
+            char = content["weather"][0]["description"]
+
+            return '''
+                    <center>
+                    <h1>The City is {}</h1>
+                    <p>The temperature is {}°</p>
+                    <p>The temperature feels like {}°</p>
+                    <p>The characteristic of the weather: {}</p>
+                    <button onClick="window.history.back()">Back</button>
+                     </center>'''.format(city, temp, feels_like, char)
+        else:
+            return '''
+             <center>
+                    <h1>Wrong city name</h1>
+                    <button onClick="window.history.back()">Back</button>
+                    </center>
+            '''
 
     return '''
               <form method="POST">
